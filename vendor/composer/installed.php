a5d383033a96f1e304876ef58d9d0ce227b5c684
SrGabrysh <gabriel@toute-belle.com>
2025-09-25 16:08:17 +0200

    fix: Mise à jour automatique du plugin WC Qualiopi Steps

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'tb-web/wc_qualiopi_steps',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'f9674b224ce5120db38dde0ea5e5e938f2d09aff',
+        'reference' => '3c8d5fcdc0ed9eb52419c72d942b726dbcd12689',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'tb-web/wc_qualiopi_steps' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'f9674b224ce5120db38dde0ea5e5e938f2d09aff',
+            'reference' => '3c8d5fcdc0ed9eb52419c72d942b726dbcd12689',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
